0b52e813c70015f1a04cc402eac50754e3957cc4
Muttukuru Rahul <muttukururahul@gmail.com>
2026-09-14 10:23:09 +0530

    Update credits in Sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -151,7 +151,7 @@ function Sidebar({ user }) {
           </button>
 
           <div className="sidebar-credits">
-            <p>© 2026 U.Do — Crafted by Rahul Muttukuru and Haneesh varma</p>
+            <p>© 2026 U.Do — Crafted by Rahul Muttukuru · Designed by Haniii/p>
           </div>
         </div>
       </aside>
